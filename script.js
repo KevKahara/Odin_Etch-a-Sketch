95bcb16acc7712aab91bfed48ffc16526d9d366f
Kevin Kahara <kaharakevin254@gmail.com>
2026-01-12 02:49:58 +0300

    Add multiple color feature when a user hovers over the grid and also a reset button

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-const div = document.querySelector('#container');
+const container = document.querySelector('#container');
 
-const rows = 16;
-const columns = 16;
-const totalCells = rows * columns;
+const cellSize = 16;
+const gridSize = cellSize * cellSize;
 
-function createGrid(totalCells) {
-    for(let i = 0; i < totalCells; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('gridItem');
-        container.appendChild(cell);
-    }
+for(let i = 0; i < gridSize; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('gridItem');
+    cell.classList.add('square');
+    container.appendChild(cell);
 }
 
-createGrid(totalCells);
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'red';
+    });
+});
+
+const button = document.querySelector('#btn');
+
+button.addEventListener('click', () => {
+    const message = prompt('What gridSize would you like?');
+    let num = parseInt(message);
+
+    if(isNaN(num)) {
+        alert('Invalid input! Please input Numbers');
+    } else if(num < 1 || num > 100) {
+        alert('Please input range between 1 and 100');
+    } else {
+        alert(num);
+    }
+
+    container.textContent = '';
+    let newSquares = num * num;
+
+    for(let i = 0; i < newSquares; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem','squares');
+        container.appendChild(gridItem);
+        let sizeItem = container.clientWidth / num;
+        gridItem.style.width = `${sizeItem}px`;
+        gridItem.style.height = `${sizeItem}px`;
+    }
+    
+    const newSquareElements = document.querySelectorAll('.squares');
+    newSquareElements.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        });
+    });
+});
